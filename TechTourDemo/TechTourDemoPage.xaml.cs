23464c4cd3ca16c0e3193254dd3a277452e5f5b8
Gustavo Barrientos <gustavo.barrientos@acidstudios.me>
2017-02-20 13:14:33 -0600

    Hello World Terminado

--- TechTourDemo/TechTourDemoPage.xaml.cs
+++ TechTourDemo/TechTourDemoPage.xaml.cs
@@ -1,12 +1,22 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace TechTourDemo
 {
 	public partial class TechTourDemoPage : ContentPage
 	{
+		private int times;
+
 		public TechTourDemoPage()
 		{
 			InitializeComponent();
+			times = 0;
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			times++;
+			DisplayAlert("Hola", $"Haz tocado el boton {times} veces", "Ok");
 		}
 	}
 }
