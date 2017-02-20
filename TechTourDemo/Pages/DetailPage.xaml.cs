01a4404884408b5dcacafcfeb7d911de70534e4a
Gustavo Barrientos <gustavo.barrientos@acidstudios.me>
2017-02-20 17:27:21 -0600

    Pagina de Detalles y ToolbarItems

---
+++ TechTourDemo/Pages/DetailPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TechTourDemo
+{
+	public partial class DetailPage : ContentPage
+	{
+		public DetailPage(TodoItem item)
+		{
+			InitializeComponent();
+
+			Title = item.Name;
+			TableSection.Title = $"Detalle de: {item.Name}";
+
+			txtName.Text = item.Name;
+			swDone.On = item.IsDone;
+
+			txtName.IsEnabled = false;
+			swDone.IsEnabled = true;
+		}
+
+		public DetailPage()
+		{
+			InitializeComponent();
+			Title = "Agregar";
+			TableSection.Title = "Agregar Item";
+		}
+
+		void Handle_Clicked(object sender, EventArgs e)
+		{
+			var item = new TodoItem 
+			{
+				Name = txtName.Text,
+				IsDone = swDone.On
+			};
+
+			MessagingCenter.Send(this, "SaveTodo", item);
+		}
+	}
+}
