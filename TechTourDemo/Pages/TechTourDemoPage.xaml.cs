48b9fd11281b7301ea6cc7b8ffb7a1c1e2974f84
Gustavo Barrientos <gustavo.barrientos@acidstudios.me>
2017-02-20 18:02:48 -0600

    Fix

--- TechTourDemo/Pages/TechTourDemoPage.xaml.cs
+++ TechTourDemo/Pages/TechTourDemoPage.xaml.cs
@@ -23,7 +23,6 @@ namespace TechTourDemo
 				new TodoItem { IsDone = true, Name = "Xamarin.Forms - Intro" },
 				new TodoItem { IsDone = true, Name = "Xamarin.Forms - Hello World" },
 				new TodoItem { IsDone = false, Name = "Xamarin.Forms - ListView" },
-				new TodoItem { IsDone = false, Name = "Xamarin.Forms - Behaviors" },
 				new TodoItem { IsDone = false, Name = "Xamarin.Forms - Data Binding" },
 				new TodoItem { IsDone = false, Name = "Xamarin.Forms - SQLite!!" }
 			});
